b346c50a182848ff1aeb111a7f8dde336589171e
YamiTS09 <20233L001040@utcv.edu.mx>
2025-03-27 07:28:52 -0600

    Creacion del proyecto en angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LandingPageComponent } from './landing/landing-page/landing-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/landing'
+  },
+  {
+    path: 'landing',
+    component: LandingPageComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './landing/footer/footer.component';
+import { HeaderComponent } from './landing/header/header.component';
+import { ContentComponent } from './landing/content/content.component';
+import { LandingPageComponent } from './landing/landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    ContentComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
